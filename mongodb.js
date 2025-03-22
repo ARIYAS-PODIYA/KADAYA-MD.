d1f7db80eab73c3f8d86186eb340eb3ab6726194
ARIYAS-PODIYA <www.freefireindunisia12@gmail.com>
2025-03-23 00:04:28 +0530

    lib

---
+++ mongodb.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const config = require('../config');
+const EnvVar = require('./mongodbenv');
+
+const defaultEnvVariables = [
+    { key: 'ALIVE_IMG', value: 'https://github.com/ARIYAS-PODIYA/-----/blob/9e3f4a06362ad04f408eb174ddb250f6a5209c24/Dofoto_20250315_191811774.jpg' },
+    { key: 'ALIVE_MSG', value: 'Hello , I am alive now!!\n\n🥶𝐌𝐚𝐝𝐞 𝐛𝐲 💚𝙺_𝙰_𝙳_𝙰_𝚈_𝙰__𝙼𝙳❤️🥶' },
+    { key: 'PREFIX', value: '.' },
+];
+
+// MongoDB connection function
+const connectDB = async () => {
+    try {
+        await mongoose.connect(config.MONGODB);
+        console.log('🛜 MongoDB Connected ✅');
+
+        // Check and create default environment variables
+        for (const envVar of defaultEnvVariables) {
+            const existingVar = await EnvVar.findOne({ key: envVar.key });
+
+            if (!existingVar) {
+                // Create new environment variable with default value
+                await EnvVar.create(envVar);
+                console.log(`➕ Created default env var: ${envVar.key}`);
+            }
+        }
+
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
